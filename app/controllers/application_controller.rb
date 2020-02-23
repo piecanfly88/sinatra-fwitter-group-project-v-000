3b558027f196622369136d18b0087f2e5eefc85c
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2020-02-23 16:31:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -62,14 +62,13 @@ class ApplicationController < Sinatra::Base
 
   helpers do
 
-    def logged_in?
-      !!current_user
-    end
-
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+    def logged_in?
+      !!current_user
+    end
   end
 
 end
